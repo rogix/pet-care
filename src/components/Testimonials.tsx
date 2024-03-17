30d84b4330eb1048c8a5a412f982514a9b770f16
Rogix <12768403+rogix@users.noreply.github.com>
2024-03-17 16:35:46 -0300

    Improve testimonials and numbers section responsive layout

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -9,14 +9,14 @@ export function Testimonials() {
 	return (
 		<section className="mb-10 lg:h-screen">
 			<div className="container relative flex h-full flex-col items-center justify-between md:flex-row">
-				<div className="flex w-full max-w-[600px] flex-col gap-5 md:w-2/4">
+				<div className="mx-auto flex w-full max-w-[500px] flex-col gap-5 xl:w-2/4 xl:max-w-[570px]">
 					<h2 className="text-teal">Glowing Coats & Wagging Tails</h2>
 					<h3 className="text-5xl text-dark-gray">Grooming Testimonials That Speak Volumes</h3>
 					<p className="font-readex text-base text-gray">
 						Our clients are our biggest supporters. Would you like to know what they think about us?
 						Take a look at the testimonials below to find out what our clients are saying about us.
 					</p>
-					<section className="h-320 flex w-full flex-col gap-5 rounded-md border-2 border-gold p-10 text-center md:w-[600px]">
+					<section className="h-320 flex w-full flex-col gap-5 rounded-md border-2 border-gold bg-off-white p-10 text-center md:w-[600px]">
 						<div className="font-readex text-2xl text-gray">
 							"I'm so grateful to Buddy for taking exceptional care of my furry friend. Their team's
 							dedication and expertise ensure my pet is always in safe hands."
@@ -30,7 +30,7 @@ export function Testimonials() {
 						</div>
 					</section>
 				</div>
-				<div className="relative hidden h-[700px] w-2/4 lg:block">
+				<div className="relative hidden h-[700px] w-2/4 xl:block">
 					<img src={Shadow} alt="" className="absolute z-0" />
 					<img src={ManDogs} alt="" className="absolute z-10" />
 					<img src={LineTestimonials} alt="line" className="absolute z-20" />
